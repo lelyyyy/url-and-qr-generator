676b763cea523cf2b916bf7337ab749fbf8fded1
lelyyyy <lelyanjai@gmail.com>
2026-01-03 03:12:54 +0700

    Update code

--- database.py
+++ database.py
@@ -1,12 +1,11 @@
 import json
 from pathlib import Path
 
-# Setup lokasi file urls.json (biar nggak nyasar)
+# Setup lokasi file urls.json (database)
 BASE_DIR = Path(__file__).resolve().parent
 DATA_FILE = BASE_DIR / "urls.json"
 
 def ambil_semua_data():
-    # Cek kalau file belum ada atau kosong, anggap dictionary kosong {}
     if not DATA_FILE.exists():
         return {}
     try:
@@ -14,20 +13,15 @@ def ambil_semua_data():
     except:
         return {}
 
-# --- [PENTING] ---
-# Fungsi ini menerima 2 bahan: code (kuncinya) dan original_url (isinya)
 def simpan_link_baru(code, original_url):
     data = ambil_semua_data()
     
-    # Validasi: Kalau kode udah ada, tolak!
     if code in data:
         return False 
     
-    # Kalau aman, simpan
     data[code] = original_url
     DATA_FILE.write_text(json.dumps(data, indent=2))
     return True
-# -----------------
 
 def cari_link_asli(code):
     data = ambil_semua_data()
